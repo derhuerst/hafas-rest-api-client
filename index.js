ed07d1bcbae198802c09cf4bbdbdd2e6b7c55c04
Jannis R <mail@jannisr.de>
2016-06-07 13:54:03 +0200

    handle UNIX timestamps :bug:

--- index.js
+++ index.js
@@ -37,9 +37,10 @@ const station = (id) => {
 
 const departures = (id, q = {}) => {
 	if ('number' !== typeof id) throw new Error('id must be a number')
+	if ('when' in q) q.when /= 1000
 	return request(`/stations/${id}/departures`, q)
 	.then((deps) => {
-		for (let dep of deps) dep.when = new Date(dep.when)
+		for (let dep of deps) dep.when = new Date(dep.when * 1000)
 		return deps
 	}, (err) => err)
 }
@@ -61,14 +62,15 @@ const routes = (from, to, q = {}) => {
 	if ('number' !== typeof to) throw new Error('to must be a number')
 	q.from = from
 	q.to = to
+	if ('when' in q) q.when /= 1000
 	return request('/routes', q)
 	.then((routes) => {
 		for (let route of routes) {
-			route.start = new Date(route.start)
-			route.end = new Date(route.end)
+			route.start = new Date(route.start * 1000)
+			route.end = new Date(route.end * 1000)
 			for (let part of route.parts) {
-				part.start = new Date(part.start)
-				part.end = new Date(part.end)
+				part.start = new Date(part.start * 1000)
+				part.end = new Date(part.end * 1000)
 			}
 		}
 		return routes
